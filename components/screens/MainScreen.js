b342aabf709e67810807deecafbf86ab04914743
ahdhani <ahmedhanitk@gmail.com>
2020-08-06 17:47:06 +0530

    Drawer heade enhanced

--- components/screens/MainScreen.js
+++ components/screens/MainScreen.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import CustomScreen from './CustomScreen/CustomScreen';
 import EditScreen from './EditSubject/EditScreen';
 import ConfigScreen from './ConfigureScreen/ConfigScreen';
 import colors from '../../constants/colors'
 
 import { NavigationContainer } from '@react-navigation/native';
-import { DrawerContentScrollView, DrawerItemList, createDrawerNavigator} from '@react-navigation/drawer';
-import { Image } from 'native-base';
+import { DrawerContentScrollView, DrawerItemList, createDrawerNavigator } from '@react-navigation/drawer';
+// import { Image } from 'native-base';
 
 
 const Drawer = createDrawerNavigator();
@@ -18,10 +18,22 @@ export default MainScreen = () => {
   const CustomDrawerContent = (props) => {
     return (
       <DrawerContentScrollView {...props}>
-        <View style={{backgroundColor: colors.color5,height: 150,marginTop: -5}} />
-        <View style={{flexDirection: 'row',marginTop: -40, paddingHorizontal: 20}} >
-          <Image source={require('../../constants/profile.jpeg')} style={{height: 50}} />
-          <Text style={{color: '#fff', fontSize: 22,fontWeight: 'bold'}}>Bunker</Text>
+        <View style={{ backgroundColor: colors.color5, height: 120, marginTop: -5 }} />
+        <View style={{
+          flexDirection: 'row', marginTop: -40,
+          paddingHorizontal: 20,marginBottom: 20
+        }} >
+          <Image source={require('../../assets/profile.jpeg')}
+            style={{
+              height: 60, width: 60,
+              borderRadius: 30, margin: 8,
+              borderWidth: 4,borderColor: '#fff'
+            }} />
+          <Text
+            style={{
+              color: '#fff', fontSize: 22,
+              fontWeight: 'bold', margin: 5
+            }}>Bunker</Text>
         </View>
 
         <DrawerItemList {...props} />
@@ -33,12 +45,12 @@ export default MainScreen = () => {
 
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home"
-      drawerContent={(props) => <CustomDrawerContent {...props} /> }
-      drawerContentOptions={{
-        activeTintColor: '#aaa',
-      }}
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
+        drawerContentOptions={{
+          activeTintColor: '#aaa',
+        }}
       >
-        <Drawer.Screen name="Home" component={CustomScreen}/>
+        <Drawer.Screen name="Home" component={CustomScreen} />
         <Drawer.Screen name="Edit Subjects" component={EditScreen} />
         <Drawer.Screen name="Configure" component={ConfigScreen} />
       </Drawer.Navigator>
